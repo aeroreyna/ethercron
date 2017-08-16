4843ed3be50b7421294bf43444bdfdf78810a780
Adolfo Reyna <aeroreyna@gmail.com>
2017-08-16 14:42:42 -0700

    initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  #app
+    .row
+      .col.s12
+        h1 Navegation:
+    router-view
+</template>
+
+<script>
+  export default {
+    name: 'etherctron'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
---
+++ src/renderer/main.js
@@ -0,0 +1,21 @@
+var $ = window.jQuery = require('jquery');
+require('materialize-css/dist/css/materialize.css')
+require('materialize-css/dist/js/materialize.js')
+//window.axios = require('axios')
+
+import Vue from 'vue'
+import axios from 'axios'
+
+import App from './App'
+import router from './router'
+
+if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+Vue.http = Vue.prototype.$http = axios
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  components: { App },
+  router,
+  template: '<App/>'
+}).$mount('#app')
